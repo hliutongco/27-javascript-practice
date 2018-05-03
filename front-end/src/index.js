903055f90ed831d2e642e0f8b9716d062096ce01
Helen Liutongco <liutongcoh@gmail.com>
2018-05-03 14:05:47 -0400

    Add the index.js and index.html code

---
+++ front-end/src/index.js
@@ -0,0 +1,71 @@
+// 4. Create DOM Content Loaded and form submit event listeners
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  const newTaskForm = document.getElementById('form')
+  const taskList = document.getElementById('taskList')
+  const newTaskDescription = document.getElementById('newTaskDescription')
+  const newTaskPriority = document.getElementById('newTaskPriority')
+
+  newTaskForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    // 5. Set up form submit event listener to create new tasks
+    const newTask = new Task({
+      id: taskStore.length,
+      description: newTaskDescription.value,
+      priority: newTaskPriority.value,
+      completed: false
+    })
+
+    // 6. Set up render task functions
+    const taskLi = newTask.createLi()
+    taskList.append(taskLi);
+
+    // 10.  Create the task in the API
+    newTask.createTask();
+  })
+
+  // 13. Set up a click event listener on the task list
+  taskList.addEventListener('click', function(event){
+    const eventId = event.target.id
+
+    // 14. If there is no eventId, exit the event listener
+    if(eventId.length === 0){
+      return
+    }
+
+    if(eventId.includes('delete-button')){
+      // 15. Find the relevant task in taskStore
+      const foundTask = taskStore.find((task) => {
+        return eventId === `delete-button-${task.id}`
+      })
+
+      // 16. Remove task from list and from the API
+      taskList.removeChild(event.target.parentElement)
+      foundTask.deleteTask()
+    }
+    else if(eventId.includes('checkbox')){
+      // 18. Find relevant task in taskStore
+      const foundTask = taskStore.find((task) => {
+        return eventId === `checkbox-${task.id}`
+      })
+
+      // 19. Edit task in the store and in the API
+      foundTask.completed ? foundTask.completed = false : foundTask.completed = true;
+      foundTask.editTask()
+    }
+  })
+
+  // 12. Set up request to fetch all existing tasks on page load
+  fetch('http://localhost:3000/tasks')
+  .then(response => response.json())
+  .then(data => {
+   for (let i of data) {
+     const newTask = new Task(i)
+     const taskLi = newTask.createLi()
+     taskList.append(taskLi)
+   }
+  })
+
+})
---
+++ front-end/src/task.js
@@ -0,0 +1,62 @@
+// 1. Create Task class, constructor, and store
+
+const taskStore = []
+
+class Task {
+  constructor(object){
+    this.id = object.id
+    this.description = object.description
+    this.priority = object.priority
+    this.completed = object.completed
+    taskStore.push(this)
+  }
+
+  // 7. Create render function
+
+  render() {
+    return `${this.description} | ${this.priority} |
+    <input id="checkbox-${this.id}" type="checkbox" ${ this.completed ? 'checked' : '' }> |
+    <button id="delete-button-${this.id}">Delete</button>`
+  }
+
+  // 8. Create createLi function
+
+  createLi(){
+    const taskLi = document.createElement('li')
+    taskLi.innerHTML = this.render()
+    return taskLi
+  }
+
+  // 11. Create createTask fetch request
+  createTask(){
+    fetch('http://localhost:3000/tasks', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(this)
+    }).then(response => response.json())
+  }
+
+  // 17. Create deleteTask fetch request
+
+  deleteTask(){
+    fetch(`http://localhost:3000/tasks/${this.id}`, {
+      method: 'DELETE'
+    })
+  }
+
+  // 20. Create editTask fetch request
+
+  editTask(){
+    fetch(`http://localhost:3000/tasks/${this.id}`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({"completed": `${this.completed}`})
+    }).then(response => response.json())
+  }
+}
